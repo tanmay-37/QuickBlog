b4e164054a82478e706d19763a9fd23b8878fec9
pahurkarsm <pahurkarsm@rknec.edu>
2025-09-04 15:44:09 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-9xl flex justify-center items-center h-screen font-bold text-blue-900'>
+        QuickBlog
+      </div>
+    </>
+  )
+}
+
+export default App
