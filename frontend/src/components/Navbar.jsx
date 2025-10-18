243e1b94455bb85f1813540ed93b5690c2f9a7d4
sanchitpahurkar <pahurkarsm@rknec.edu>
2025-10-18 23:05:20 +0530

    Responsive Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,22 +1,44 @@
-import React from 'react'
+import { useState } from "react";
 
 const Navbar = () => {
+
+    const [isOpen, setIsOpen] = useState(false)
+
   return (
     <nav className='font-sans'>
-      <div className='flex justify-between px-15 py-5'>
+
+      {/* for large devices */}
+      <div className='flex justify-between items-center md:px-15 px-5 py-5'>
         <a href="/"><img src="/logo.svg" alt="" /></a>
-        <a href="/admin">
-            <button 
-            className='bg-brand-purple text-white flex items-center justify-center gap-1 text-sm font-medium px-6 py-2 rounded-4xl cursor-pointer hover:transform hover:-translate-y-0.5 shadow-lg'
-            >
-                Admin Login
-                <img 
-                src="/lock.svg" 
-                className='h-5 pb-1'
-                alt="" 
-                /> 
-            </button>
-        </a>
+
+        {/* desktop button */}
+        <div className="hidden md:block">
+            <a href="/admin">
+                <button 
+                className='bg-brand-purple text-white flex items-center justify-center gap-1 text-sm font-medium px-6 py-2 rounded-4xl cursor-pointer hover:transform hover:-translate-y-0.5 shadow-lg'
+                >
+                    Admin Login
+                    <img 
+                    src="/lock.svg" 
+                    className='h-5 pb-1'
+                    alt="" 
+                    /> 
+                </button>
+            </a>
+        </div>
+
+        {/* hamburgir icon */}
+        <div className="md:hidden">
+            <a href="/admin">
+                <button className="bg-brand-purple py-2 px-4 rounded-2xl">
+                    <img 
+                        src="/lock.svg" 
+                        className='h-5 pb-1'
+                        alt="" 
+                    /> 
+                </button>
+            </a>
+        </div>
       </div>
     </nav>
   )
