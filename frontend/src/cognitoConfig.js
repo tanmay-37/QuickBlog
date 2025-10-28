809b2a03fdcbb91f14bbe33f5dc19f92e777461f
tanmay-37 <talekartk@rknec.edu>
2025-10-28 20:46:14 +0530

    cognito config

--- frontend/src/cognitoConfig.js
+++ frontend/src/cognitoConfig.js
@@ -3,7 +3,7 @@ const COGNITO_CONFIG = {
   userPoolId: import.meta.env.VITE_COGNITO_USER_POOL_ID,
   clientId: import.meta.env.VITE_COGNITO_CLIENT_ID,
   domain: import.meta.env.VITE_COGNITO_DOMAIN, 
-  redirectUri: "http://localhost:5173/",
+  redirectUri: import.meta.env.VITE_COGNITO_REDIRECT_URI,
   scopes: ["email", "openid", "phone"],
 };
 
